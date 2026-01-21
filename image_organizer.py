820e912d97085d863f2680ed2c1732191134b785
AizazKhan19 <aizazkhan7874@gmail.com>
2026-01-21 22:57:18 +0500

    Subfolders for cats and dogs images are created

---
+++ image_organizer.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+
+# Root directory where images are stored
+pet_images_path= 'data/PetImages'
+
+# creating subfolders for cats and dogs
+cat_folder = os.path.join(pet_images_path, 'Cat')
+dog_folder = os.path.join(pet_images_path, 'Dog')
+
+os.makedirs(cat_folder, exist_ok =  True)
+os.makedirs(dog_folder, exist_ok =  True)
+
+# Moving images to their respective folders
+for img in os.listdir(pet_images_path):
+    if img.startswith('cat'):
+        shutil.move(os.path.join(pet_images_path, img), os.path.join(cat_folder, img))
+    elif img.startswith('dog'):
+        shutil.move(os.path.join(pet_images_path, img), os.path.join(dog_folder, img))
+
+print('Images have been organized into cat and dog folders')
