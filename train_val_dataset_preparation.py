aaa5bf5be86f296550b46f74b295a6d6a433cb50
Aizaz Khan <aizazkhan7874@gmail.com>
2026-01-25 23:05:13 +0500

    Splitting dataset into training and validation datasets

---
+++ train_val_dataset_preparation.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+# creating dataset for training and validation
+# Below function will return two datasets. One is training dataset and the second will be validation dataset.
+
+def train_val_dataset_preparation():
+
+    training_dataset = tf.keras.utils.image_dataset_from_directory(
+        directory = './data/Petimages',  
+        image_size = (150, 150),
+        batch_size = (128),
+        label_mode = 'binary',
+        validation_split = 0.1,
+        subset = 'training',
+        seed = 42
+
+    )
+
+    validation_dataset = tf.keras.utils.image_dataset_from_directory(
+        directory = './data/PetImages',
+        image_size = (150, 150),
+        batch_size = (128),
+        label_mode = 'binary',
+        validation_split = 0.1,
+        subset = 'validation',
+        seed = 42
+    )
+
+
+    return training_dataset, validation_dataset
+
+# creating training and validation datasets
+
+training_dataset, validation_dataset = train_val_dataset_preparation()
