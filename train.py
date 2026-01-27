1fa94a6790faa6b5dda00cc84f3b706595ae2ed4
Aizaz Khan <aizazkhan7874@gmail.com>
2026-01-28 00:08:12 +0500

    training of model

---
+++ train.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from train_val_dataset_preparation import train_val_dataset_preparation
+from Callbacks import Earlystoppingcallback
+from creating_and_compiling_model import create_and_compile_model
+
+# getting training and validation dataset
+training_dataset, validation_dataset = train_val_dataset_preparation()
+
+# get untrained model 
+model= create_and_compile_model()
+
+# now lets train the model using fit method
+def training_model():
+    history = model.fit(
+        training_dataset,
+        epochs = 15,
+        validation_data= validation_dataset,
+        callbacks= [Earlystoppingcallback()]
+    )
+
+
+if __name__ == '__main__':
+    training_model()
